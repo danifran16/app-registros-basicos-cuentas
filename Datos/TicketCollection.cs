2da0d6e7cac700a9488805d96f3075555e0c6485
Asus <Asus@LAPTOP-J61EJ5OK>
2023-11-12 22:24:22 -0300

    Agregar archivos de proyecto.

---
+++ Datos/TicketCollection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    static class TicketCollection
+    {
+        public static List<ListaTickets> listado_tickets = new List<ListaTickets>();
+            
+    }
+}
---
+++ Modelo/Ticket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public Cliente? Cliente { get; set; }
+        public string? Producto { get; set; }
+        public string? Descripcion { get; set; }
+        public string? Estado { get; set; }
+        private DateTime? _createdAt { get; set; } = DateTime.Now;
+    }
+}
